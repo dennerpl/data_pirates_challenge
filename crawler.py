fcbbbc2cb2dd741d309e2f6e3bc2dbac82ac96c3
unknown <eteg@raiden.sede.eteg.net>
2022-04-01 19:49:55 -0300

    faz a requisicao da pagina inicial de varios estados

--- crawler.py
+++ crawler.py
@@ -2,37 +2,53 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+states = [
+   'SP',
+   'MG' 
+]
+
 result = {
-    'UF': ["SP", "MG"],
-    'Localidade': ["São Paulo", "Belo Horizonte"],
-    'CEP': [33221564, 21465878],
-    'Situacao': ['teste', 'teste'],
-    'Faixa': ['faixa1', 'faixa2']
+    'UF': [],
+    'Localidade': [],
+    'CEP': [],
+    'Situacao': [],
+    'Tipo': []
 }
 
-next_key = 'Localidade'
 
-response = requests.post('https://www2.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm', data={'UF': 'SP', 'qtdrow': 50, 'pagini': 51, 'pagfim': 100})
+def scrapper(response, uf):
+    next_key = 'Localidade'
+    site = BeautifulSoup(response.content, 'html.parser')
 
-site = BeautifulSoup(response.content, 'html.parser')
+    tabelas = site.select('table.tmptabela')
 
-tabelas = site.select('table.tmptabela')
+    if len(tabelas) > 1:
+        infos = tabelas[1].select('table.tmptabela tr td')
+    else:
+        infos = site.select('table.tmptabela tr td')
 
-infos = site.select('table.tmptabela tr td')
+    for info in infos:
+        result[next_key].append(info.text)
+        if next_key == 'Localidade':
+            result['UF'].append(uf)
+            next_key = 'CEP'
+        elif next_key == 'CEP':
+            next_key = 'Situacao'
+        elif next_key == 'Situacao':
+            next_key = 'Tipo'
+        elif next_key == 'Tipo':
+            next_key = 'Localidade'
 
-uf = site.find_all('td', width=10)
+def post_request(uf, pagini, pagfim):
+    try:
+        response = requests.post('https://www2.correios.com.br/sistemas/buscacep/resultadoBuscaFaixaCEP.cfm', data={'UF': 'SP', 'qtdrow': 50, 'pagini': pagini, 'pagfim': pagfim})
+    except:
+        print('Parâmetros da requisição incorretos')
+    return response
 
-for info in infos:
-    result[next_key].append(info.text)
-    if next_key == 'Localidade':
-        result['UF'].append('SP')
-        next_key = 'CEP'
-    elif next_key == 'CEP':
-        next_key = 'Situacao'
-    elif next_key == 'Situacao':
-        next_key = 'Faixa'
-    elif next_key == 'Faixa':
-        next_key = 'Localidade'
+for state in states:
+    request_result = post_request(state, 1, 50)
+    scrapper(request_result, state)
 
 data = pd.DataFrame(result)
 
